8e2a585fb7cea62176123a5d142b53ccb23e5315
Lucas Burlingham <lucas.burlingham@icloud.com>
2020-10-08 21:39:51 -0400

    Format Document

--- runAsRoot.c
+++ runAsRoot.c
@@ -2,17 +2,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-  int
-  main (int argc, char *argv[])
-  {
-     setuid (0);
+int main(int argc, char *argv[])
+{
+  setuid(0);
 
-     /* WARNING: Only use an absolute path to the script to execute,
+  /* WARNING: Only use an absolute path to the script to execute,
       *          a malicious user might fool the binary and execute
       *          arbitary commands if not.
       * */
 
-     system ("/bin/sh /path/to/php_shell.sh");
+  system("/bin/sh /path/to/php_shell.sh");
 
-     return 0;
-   }
+  return 0;
+}
